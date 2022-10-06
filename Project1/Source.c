566c3a74381e2d4d8fef68c92c3c0b792fe9deb3
СорокинДА <sorokindima36@gmail.com>
2022-10-06 16:37:47 +0300

    NE RABOTAET (((((((((

--- Project1/Source.c
+++ Project1/Source.c
@@ -9,12 +9,12 @@ struct list
 typedef struct list* LOS;
 LOS create(int n)
 {
-	LOS los = calloc(1, sizeof(LOS));
+	LOS los = calloc(1, sizeof(struct list));
 	LOS p1 = los, p2;
 	p1->a = 1;
 	for (size_t i = 0; i < n-1; i++)
 	{
-		p2= calloc(1, sizeof(LOS));
+		p2= calloc(1, sizeof(struct list));
 		p1->next = p2;
 		p2->a = p1->a + 1;
 		p1 = p2;
@@ -33,23 +33,38 @@ void print(LOS los)
 }
 LOS deliteElement(LOS los, int n)
 {
-	if (n == 1)
-	{	
-		los->next = los;
-		LOS p1 = los;
-		
-	}
-	else
-	{
-		for (size_t i = 0; i < n - 2; i++)
+
+		for (size_t i = -2; i < n; i++)
 		{
 			los = los->next;
 		}
 		LOS p1 = los;
 		los->next = los->next->next;
-		//free(p1);
-	}
 
+}
+LOS zamena(LOS los, int a, int b)
+{
+	LOS gol = los;
+	LOS p1=0;
+	LOS p2;
+	los = gol;
+	for (int i = -2; i < b-4; i++)
+	{
+		los = los->next;
+		if (i == a)
+		{
+			p1 = los;
+		}
+	}
+	p2 = los;
+	los= p1;
+	los = gol;
+	for (int i = -2; i < a-4; i++)
+	{
+		los = los->next;
+	}
+	los = p2;
+	return los;
 }
 void InsertElement(LOS los, LOS item, int n)
 {
@@ -70,17 +85,14 @@ void InsertElement(LOS los, LOS item, int n)
 }
 LOS deliteAll(LOS los)
 {
-	int j = N;
-	for (size_t b = 0; b < j - 2; b++)
+	LOS ind = los;
+	while (ind)
 	{
-		for (size_t i = 0; i < j - 2; i++)
-		{
-			los = los->next;
-		}
-		LOS p1 = los;
-		los->next = los->next->next;
-		j--;
-	}		
+		ind = los->next;
+		free(los);
+		los = ind;
+	}
+
 }
 
 int main()
@@ -88,15 +100,19 @@ int main()
 	//LOS a = malloc(sizeof(LOS));
 	//LOS b = malloc(sizeof(LOS));
 	LOS los = create(N);
+	LOS s = los;
 	print(los);
+	los = s;
 	deliteElement(los, 1);
 	print(los);
-	LOS item = malloc(sizeof(LOS));
+	los = s;
+	
+	print(zamena(los, 2, 5));
+	/*LOS item = malloc(sizeof(struct list));
 	item->a = 777;
 	item->next = NULL;
 	InsertElement(los, item, 4);
-	print(los);
-	//deliteAll(los);
-	//print(los);
+	print(los);*/
+	deliteAll(los);
 	return 0;
 }
